79f90658733502acb413d757fbb45e6dace9ad06
RudnevDm <www.rudnev.web@gmail.com>
2017-03-03 02:04:13 +0300

    Внес правки в соответствии с рекомендациями наставника

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -2,58 +2,29 @@
 
 
   window.synchronizeFields = function (
-    syncFieldOne,
-    syncFieldTwo,
-    syncCollectionOne,
-    syncCollectionTwo,
-    syncFieldValue) {
+      syncFieldOne,
+      syncFieldTwo,
+      syncCollectionOne,
+      syncCollectionTwo,
+      syncFieldValue) {
 
 
-    var currentField = null;
-    var currentValue = null;
-
-
-    var checkTypeField = function (fieldElement, fieldElementValue) {
-      if (fieldElement.options) {
-        for (var i = 0; i < fieldElement.options.length; i++) {
-          if (fieldElement.options[i].value === fieldElementValue) {
-            fieldElement.options[i].selected = true;
-            return fieldElement.options[i];
-          }
-        }
-      } else {
-        return fieldElement;
-      }
-    };
-
-
-    syncFieldOne.addEventListener('change', function(event) {
-
-      currentField = null;
-
-      currentValue = event.target.value || event.target.options[event.target.selectedIndex].value;
+    syncFieldOne.addEventListener('change', function (event) {
 
       for (var i = 0; i < syncCollectionOne.length; i++) {
-        if (parseInt(syncCollectionOne[i], 10) === parseInt(currentValue), 10) {
-          currentField = checkTypeField(syncFieldTwo, currentValue);
-          console.log(currentField);
-          currentField.value = syncCollectionOne[i];
+        if (syncFieldOne[syncFieldValue] === syncCollectionOne[i]) {
+          syncFieldTwo[syncFieldValue] = syncCollectionTwo[i];
         }
       }
 
     });
 
 
-    syncFieldTwo.addEventListener('change', function(event) {
+    syncFieldTwo.addEventListener('change', function (event) {
 
-      currentField = null;
-
-      currentValue = event.target.value || event.target.options[event.target.selectedIndex].value;
-
-      for (var c = 0; c < syncCollectionTwo.length; c++) {
-        if (parseInt(syncCollectionTwo[c], 10) === parseInt(currentValue, 10)) {
-          currentField = checkTypeField(syncFieldOne, currentValue);
-          currentField.value = syncCollectionTwo[c];
+      for (var i = 0; i < syncCollectionOne.length; i++) {
+        if (syncFieldTwo[syncFieldValue] === syncCollectionTwo[i]) {
+          syncFieldOne[syncFieldValue] = syncCollectionOne[i];
         }
       }
 
