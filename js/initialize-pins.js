f7c118c5731eb8bfd3e79d001e2777f73994f7fb
RudnevDmitry <www.rudnev.web@gmail.com>
2017-03-10 21:32:54 +0300

    Внес правки в соответствии с рекомендациями наставника

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -25,50 +25,122 @@
       focusPin = cb;
 
 
-      var onload = function (data) {
-        for (var i = 0; i < data.length; i++) {
-          similarApartments.push(data[i]);
+      var cleanMap = function (map, collection) {
+        for (var i = 0; i < collection.length; i++) {
+          if (collection[i].classList.contains('pin__main')) {
+            continue;
+          } else {
+            map.removeChild(collection[i]);
+          }
         }
       };
 
 
       var getFilterValues = function () {
-        pinElements = pinElementsWrapper.querySelectorAll('.pin');
-        cleanMap(pinMap, pinElements);
         filterValues = [];
         for (var i = 0; i < filterFields.length; i++) {
           if (filterFields[i].options) {
             filterValues.push(filterFields[i].options[filterFields[i].selectedIndex].value);
           }
         }
-        pinFilter(filterValues);
+      };
+
+      var objProps = [
+        'price',
+        'type',
+        'rooms',
+        'guests'
+      ];
+
+
+      var currentProps = [];
+      var deletedItem = [];
+
+      var findProp = function (collection) {
+        var i = 0;
+        for (var variable in collection.offer) {
+          if (collection.offer.hasOwnProperty(variable)) {
+            if (variable === objProps[i]) {
+              var prop = variable + ':' + collection.offer[variable];
+              currentProps.push(prop.split(':')[1]);
+              i++;
+            }
+          }
+        }
+        deletedItem = [];
+        deletedItem = currentProps.splice(1, 1);
+        currentProps.splice(0, 0, deletedItem[0]);
       };
 
 
-      var pinFilter = function (collection) {
-        var currentProp;
-        for (var i = 0; i < similarApartments.length; i++) {
-          for (var variable in similarApartments[i].offer) {
-            if (similarApartments[i].offer.hasOwnProperty(variable)) {
-              currentProp = variable + ' = ' + similarApartments[i].offer[variable];
-              collection.length = similarApartments.length;
-              currentProp = currentProp.split('=')[1];
-              for (var c = 0; c < collection.length; c++) {
-                if (collection[c] === currentProp.split(' ')[1]) {
-                  pinMap.appendChild(window.render(similarApartments[i]));
-                  break;
-                } else if (collection[c] === 'any' && currentProp.split(' ')[1] === '0') {
-                  pinMap.appendChild(window.render(similarApartments[i]));
-                  break;
-                }
-              }
+      var flag = 0;
+
+
+      var parseValues = function (str, count) {
+        if (str === 'any') {
+          return count;
+        }
+        switch (true) {
+          case str === 'low':
+            if (+count > 0 && +count < 10000) {
+              return count;
             }
+            break;
+          case str === 'middle':
+            if (+count >= 10000 && +count < 50000) {
+              return count;
+            }
+            break;
+          case str === 'hight':
+            if (+count >= 50000 && +count < 100000000) {
+              return count;
+            }
+            break;
+        }
+        return str;
+      };
+
+
+      var setProp = function (collection, formData, obj) {
+        for (var i = 0; i < collection.length; i++) {
+          if (collection[i] === parseValues(formData[i], collection[i])) {
+            flag++;
           }
+        }
+        if (flag === 4) {
+          pinMap.appendChild(window.render(obj));
+        }
+        pinElements = pinElementsWrapper.querySelectorAll('.pin');
+        i = 0;
+        flag = 0;
+        currentProps = [];
+        pinElements = pinElementsWrapper.querySelectorAll('.pin');
+      };
+
+
+      var pinFilter = function () {
+        cleanMap(pinMap, pinElements);
+        currentProps = [];
+        getFilterValues();
+
+        for (var i = 0; i < similarApartments.length; i++) {
+          if (currentProps.length === 4) {
+            setProp(currentProps, filterValues, similarApartments[i - 1]);
+          }
+          findProp(similarApartments[i]);
         }
       };
 
 
-      filterFields.addEventListener('change', getFilterValues);
+      filterFields.addEventListener('change', pinFilter);
+
+
+      var onload = function (data) {
+        for (var i = 0; i < data.length; i++) {
+          similarApartments.push(data[i]);
+        }
+        pinFilter();
+      };
 
 
       var errorHandler = function (err) {
@@ -82,17 +154,6 @@
       var setAttribute = function (collection, attributeName, attributeCount) {
         for (var i = 0; i < collection.length; i++) {
           collection[i].setAttribute(attributeName, attributeCount);
-        }
-      };
-
-
-      var cleanMap = function (map, collection) {
-        for (var i = 0; i < collection.length; i++) {
-          if (collection[i].classList.contains('pin__main')) {
-            continue;
-          } else {
-            map.removeChild(collection[i]);
-          }
         }
       };
 
